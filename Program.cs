49da0149acc410d59f3d5973c2dff64ec4b9f435
user <user@LAPTOP-2DK3J1EI>
2023-09-20 18:30:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese la base: ");
+int bas = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el exponente: ");
+int expo = int.Parse(Console.ReadLine());
+int resultado = 1;
+
+for  (int i = 0; i < expo; i++)
+{
+    resultado *= bas;
+}
+Console.Write("La potencia es " + resultado);
